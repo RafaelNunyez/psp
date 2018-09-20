09f0de943c1666338261e80e18b05645ff68081c
rafaelnunyez <rafa97burja@gmail.com>
2018-09-20 10:20:49 +0200

    Corrected Code

--- HolaMundo4.java
+++ HolaMundo4.java
@@ -3,27 +3,21 @@ import java.io.*;
 
 class HolaMundo4{
     public static void main(String [] args) {
-    	int num=0;
     	String nombre;
         
     	Scanner tcl = new Scanner (System.in);
-    	System.out.println("Dime tu nombre");
+    	System.out.print("Dime tu nombre");
     	nombre = tcl.nextLine();
-    	while(num < 20) {
-    		System.out.println("Hola " + nombre);
-    		num = num + 1;     
-    		try {
-    			BufferedWriter bw = new BufferedWriter(new FileWriter(new File("fichero.txt")));
-    			for(int i = 0; i < 20; i++){
-
-    				bw.write("Hola " + nombre);;
-    				bw.newLine();
-    			}
-    			bw.close();
-    		}
-    		catch (IOException errorDeFichero) {
-    			System.out.println("Ha habido problemas: " + errorDeFichero.getMessage() );
-    		}
-    	}
+    	try {
+			BufferedWriter bw = new BufferedWriter(new FileWriter(new File("fichero.txt")));
+			for(int i = 0; i < 20; i++){
+				System.out.println("Hola " + nombre);
+				bw.write("Hola " + nombre);
+				bw.newLine();
+			}
+			bw.close();
+		} catch (IOException errorDeFichero) {
+			System.out.println("Ha habido problemas: " + errorDeFichero.getMessage() );
+		}
     }
 }
